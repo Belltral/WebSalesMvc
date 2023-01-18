86b80b9506046657b6b41c211bc66db4d07bdbd5
Henrique Reis <henriquereis198@outlook.com>
2023-01-18 20:15:04 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Index()
         {
+            //ViewData["Tristeza"] = "Não sei o que tô fazendo aqui... :(";
             return View();
         }
 
