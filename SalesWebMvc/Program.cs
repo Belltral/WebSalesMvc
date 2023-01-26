b33bdd853dff4e0bad22610f409f5f35049ac823
Henrique Reis <henriquereis198@outlook.com>
2023-01-26 20:18:28 -0300

    SellerService and basic FindAll

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,12 +13,15 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
     /* ?? throw new InvalidOperationException("Connection string 'SalesWebMvcContext' not found.")));*/
 
 // Add services to the container.
-builder.Services.AddControllersWithViews(); //Adicionado devido ao .NET 6
+builder.Services.AddControllersWithViews();
 
-builder.Services.AddScoped<SeedingService>();
+builder.Services.AddScoped<SeedingService>();  //Adicionado devido ao .NET 6
+builder.Services.AddScoped<SellerService>(); //Adicionado devido ao .NET 6
 
 var app = builder.Build();
-app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed(); //Adicionado devido ao .NET 6
+app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed(); /* Adicionado devido ao .NET 6
+                                                                                           para o serviço inserir os dados 
+                                                                                           no banco */
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
