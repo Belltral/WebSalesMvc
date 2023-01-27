0f9bf816470c78de90d8a1530ce90cb48492a2c8
Henrique Reis <henriquereis198@outlook.com>
2023-01-27 20:46:46 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -19,6 +19,7 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj); //Adiciona os itens do formulário no banco
             _context.SaveChanges(); //Salva as adições
         }
