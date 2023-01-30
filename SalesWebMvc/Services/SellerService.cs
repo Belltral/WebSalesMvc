ab3b6376cebea45df4d08267c9f180497536c283
Henrique Reis <henriquereis198@outlook.com>
2023-01-30 20:17:46 -0300

    Delete seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -22,5 +22,17 @@ namespace SalesWebMvc.Services
             _context.Add(obj); //Adiciona os itens do formulário no banco
             _context.SaveChanges(); //Salva as adições
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
     }
 }
